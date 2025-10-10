93fc806cf001382473215f8547bab410d835e625
Fred120-code <fredayemtsa@gmail.com>
2025-10-10 17:25:13 +0100

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div>
+      <div className="flex items-center justify-center flex-col py-10 w-full">
+        <div>
+          <div className="flex flex-col">
+            <h1>Vos <span>finances</span>, </h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
