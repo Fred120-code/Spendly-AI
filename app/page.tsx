e197d9f91d88149cd7300b6d14ae7304e21c9dce
Fred120-code <fredayemtsa@gmail.com>
2025-10-10 17:53:58 +0100

    creation de la lading page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,35 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
       <div className="flex items-center justify-center flex-col py-10 w-full">
+        <div className="flex items-center justify-around gap-20 w-full mb-10 p-10 h-20">
+          <h1 className="text-[#E0FF67] font-bold text-xl">Spendly AI</h1>
+          <div className="flex gap-3">
+            <button className="btn btn-primary btn-soft btn-sm md:btn-md  rounded-2xl">
+              <Link href={""}>Se connecter</Link>
+            </button>
+            <button className="btn btn-primary btn-soft btn-sm md:btn-md  rounded-2xl">
+              <Link href={""}>S'inscrire</Link>
+            </button>
+          </div>
+        </div>
         <div>
-          <div className="flex flex-col">
-            <h1>Vos <span>finances</span>, </h1>
+          <div className="flex flex-col text-center">
+            <h1 className="font-bold text-white md:text-3xl">
+              Vos <span className="text-[#E0FF67]">finances</span>,réinventer
+              par <br />
+              "l'intelligene artificielle"{" "}
+            </h1>
+            <p className="font-bold text-white py-10 sm:text-sm md:text-2xl">
+              <span className="text-[#E0FF67]">Spendly AI</span> vous aide à
+              suivre vos dépenses, comprendre vos
+              <br /> habitudes et prédire vos besoins futurs — automatiquement.
+            </p>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
